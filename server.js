07465c4daca166038f6366e8b9f3e731d97c23c6
Victor <victortang.com@gmail.com>
2019-09-05 15:49:45 +0800

    Heroku fix

--- server.js
+++ server.js
@@ -62,13 +62,15 @@ app.use(function(req, res, next) {
 });
 
 // Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, 'client', 'build', 'index.html')
-  );
-});
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
+  });
+}
 
 const port = process.env.PORT || 5000;
 
